60cc84f9ae10d7bae83ad94748451db04c6f5ae6
Benedikt Þorri Þórarinsson <benedikt@surfdis.hir.is>
2017-10-19 16:33:25 +0000

    CalcWeb modifications made on TA's end

--- src/main/java/is/ru/hugb/calc/CalcWeb.java
+++ src/main/java/is/ru/hugb/calc/CalcWeb.java
@@ -4,6 +4,7 @@ import static spark.Spark.*;
 
 public class CalcWeb {
     public static void main(String[] args) {
+        port(getHerokuPort());
         get("/", (req, res) -> {
             return "No route specified. Try /add/1,2";
         });
@@ -11,6 +12,14 @@ public class CalcWeb {
             "/add/:input",
             (req, res) -> add(req.params(":input"))
         );
+    }
+
+    static int getHerokuPort() {
+        ProcessBuilder psb = new ProcessBuilder();
+    if (psb.environment().get("PORT") != null) {
+        return Integer.parseInt(psb.environment().get("PORT"));
+    }
+    return 4567;
     }
 
     private static int add(String input) {
